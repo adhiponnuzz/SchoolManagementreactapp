8d9b90bdd8ce0cfcb0ed03e350f7cf60c8219d6c
Adhiponnuzz <adithyaminnu689693@gmail.com>
2022-05-25 20:52:31 +0530

    header finished

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Addstudent from './component/Addstudent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Searchstudent from './component/Searchstudent';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Addstudent/>
-    <Searchstudent/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Addstudent/>}/>
+      <Route path="/search" exact element={<Searchstudent/>}/>
+
+
+
+    </Routes>
+    
+    
+    
+    </BrowserRouter>
     
     
     </>
